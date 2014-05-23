e259410742e3d2251ac7e3e0182f753d01095f3d
TheInfoCoder <dylanmcpeek12@gmail.com>
2014-05-23 16:02:27 -0500

    Change help command a little

--- src/me/zodaxium/zodaxportals/commands/Commandhelp.java
+++ src/me/zodaxium/zodaxportals/commands/Commandhelp.java
@@ -9,8 +9,8 @@ public class Commandhelp extends AbstractCommand{
 	@Override
 	public void execute(Player p, String[] args) {
 		p.sendMessage(Reference.colorize("&7------------ [&a ZodaxPortals &7] ------------"));
-		p.sendMessage(Reference.colorize("&7- &a/portal help &f- &7Display this"));
-		p.sendMessage(Reference.colorize("&7- &a/portal list &f- &7List all portals"));
+		p.sendMessage(Reference.colorize("&7- &a/portal help &f- &7Display this menu"));
+		p.sendMessage(Reference.colorize("&7- &a/portal list &f- &7List all known portals"));
 		p.sendMessage(Reference.colorize("&7- &a/portal info (portal) &f- &7Get info on a portal"));
 		p.sendMessage(Reference.colorize("&7- &a/portal create (portal) (destination) &f- &7Create a portal"));
 		p.sendMessage(Reference.colorize("&7- &a/portal delete (portal) &f- &7Delete a portal"));
